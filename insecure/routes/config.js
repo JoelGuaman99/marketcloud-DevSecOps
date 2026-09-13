5baad5cc9d73c5f7334ab145d0ad5cc3368820e8
JOEL GUAMAN <joguamanoc@uide.edu.ec>
2026-09-13 08:33:35 -0500

    Agregar deteccion y correccion de secretos

---
+++ insecure/routes/config.js
@@ -0,0 +1,7 @@
+const config = {
+    databaseHost: "db.marketcloud.local",
+    databaseUser: "marketcloud_app",
+    databasePassword: "MarketCloudDemoPassword123!"
+};
+
+module.exports = config;
---
+++ secure/routes/config.js
@@ -0,0 +1,7 @@
+const config = {
+    databaseHost: process.env.DB_HOST,
+    databaseUser: process.env.DB_USER,
+    databasePassword: process.env.DB_PASSWORD
+};
+
+module.exports = config;
